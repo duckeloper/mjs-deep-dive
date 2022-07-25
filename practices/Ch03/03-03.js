cab1ec71d52c09c7f41dcd7cad46934348141de5
Byungchan Park <duckeloper@gmail.com>
2022-07-25 11:34:34 +0900

    chapter 05 start

--- practices/Ch03/03-03.js
+++ practices/Ch03/03-03.js
@@ -3,4 +3,4 @@ const arr = [1, 2, 3];
 arr.forEach(alert);
 
 // Node.js로 실행시 "ReferenceError: alert is not defined" 에러 발생
-// 클라이언트 사이트 Web API는 Node.js 환경에서 실행할 수 없음
+// 클라이언트 사이드 Web API는 Node.js 환경에서 실행할 수 없음
